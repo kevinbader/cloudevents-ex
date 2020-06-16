cf2d0a72510d78656b05eaf2fc75bb0be6ee8254
Kevin Bader <kevin.c.bader@gmail.com>
2020-06-16 22:46:08 +0200

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Cloudevents.MixProject do
     [
       app: :cloudevents,
       description: description(),
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
